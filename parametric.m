49503eaafc13f87b4234321804c86942c00cda92
dmitry <kondra2lp@gmail.com>
2011-11-08 16:20:44 +0400

    3sigma

--- parametric.m
+++ parametric.m
@@ -5,12 +5,12 @@ function [mu_, sigma_] = parametric(X, mu, sigma)
     clf;
     
     f = @(x) (1/(sqrt(2*pi)*sigma_))*exp(-(x-mu_).^2./(2*sigma_^2));
-    ezplot(f, [-10 10]);
+    ezplot(f, [mu_-3*sigma_, mu_+3*sigma_]);
     
     if nargin == 3
         f_ = @(x) (1/(sqrt(2*pi)*sigma))*exp(-(x-mu).^2./(2*sigma^2));
         hold on;
-        h = ezplot(f_, [-10, 10]);
+        h = ezplot(f_, [mu-3*sigma, mu+3*sigma]);
         set(h, 'Color', 'red');
         hold off;
     end
